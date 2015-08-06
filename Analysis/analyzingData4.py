1e54b2420a145588e198f46fae5ada65943a959d
Suryoday Basak <suryodaybasak@gmail.com>
2015-08-06 17:36:47 +0530

    Displaying all cross correlation coefficients

--- Analysis/analyzingData4.py
+++ Analysis/analyzingData4.py
@@ -50,36 +50,82 @@ class dataSetEssentials(object):
         self.lowBeta_mean, self.lowBeta_median, self.lowBeta_variance = essentialStats(self.lowBeta)
         self.highBeta_mean, self.highBeta_median, self.highBeta_variance = essentialStats(self.highBeta)
         self.lowGamma_mean, self.lowGamma_median, self.lowGamma_variance = essentialStats(self.lowGamma)
-        self.MidGamma_mean, self.MidGamma_median, self.MidGamma_variance = essentialStats(self.midGamma)
+        self.midGamma_mean, self.midGamma_median, self.midGamma_variance = essentialStats(self.midGamma)
         self.Delta_mean, self.Delta_median, self.Delta_variance = essentialStats(self.delta)
         self.Theta_mean, self.Theta_median, self.Theta_variance = essentialStats(self.theta)      
         
-
-        
     def showEssentialStats(self):
-        print np.correlate(self.lowAlpha,self.highAlpha)
-        print "\n\n",self.dataSetName,"\n\n"
+
+        print "\n\n","SHOWING ESSENTIAL STATS FOR",self.dataSetName,"\n\n"
         print "\tMEAN","\t\t","MEDIAN","\t","VARIANCE\n"
         print "Low Alpha\t",self.lowAlpha_mean,"\t",self.lowAlpha_median,"\t",self.lowAlpha_variance
         print "High Alpha\t",self.highAlpha_mean,"\t",self.highAlpha_median,"\t",self.highAlpha_variance
         print "Low Beta\t",self.lowBeta_mean,"\t",self.lowBeta_median,"\t",self.lowBeta_variance
         print "High Beta\t",self.highBeta_mean,"\t",self.highBeta_median,"\t",self.highBeta_variance
         print "Low Gamma\t",self.lowGamma_mean,"\t",self.lowGamma_median,"\t",self.lowGamma_variance
-        print "Mid Gamma\t",self.MidGamma_mean,"\t",self.MidGamma_median,"\t",self.MidGamma_variance
+        print "Mid Gamma\t",self.midGamma_mean,"\t",self.midGamma_median,"\t",self.midGamma_variance
         print "Delta\t",self.Delta_mean,"\t",self.Delta_median,"\t",self.Delta_variance
         print "Theta\t",self.Theta_mean,"\t",self.Theta_median,"\t",self.Theta_variance
+        print "\n\n"
+        
+    def attributeCorrelation(self):
+        print "CORRELATIONS:\n"
+        print "Low-Alpha|High-Alpha:\t",np.correlate(self.lowAlpha,self.highAlpha)[0]
+        print "Low-Alpha|Low-Beta:\t",np.correlate(self.lowAlpha,self.lowBeta)[0]
+        print "Low-Alpha|High-Beta:\t",np.correlate(self.lowAlpha,self.highBeta)[0]
+        print "Low-Alpha|Low-Gamma:\t",np.correlate(self.lowAlpha,self.lowGamma)[0]
+        print "Low-Alpha|Mid-Gamma:\t",np.correlate(self.lowAlpha,self.midGamma)[0]
+        print "Low-Alpha|Delta:\t",np.correlate(self.lowAlpha,self.delta)[0]
+        print "Low-Alpha|Theta:\t",np.correlate(self.lowAlpha,self.theta)[0]
+        print "\n"
+        
+        print "High-Alpha|Low-Beta:\t",np.correlate(self.highAlpha,self.lowBeta)[0]
+        print "High-Alpha|High-Beta:\t",np.correlate(self.highAlpha,self.highBeta)[0]
+        print "High-Alpha|Low-Gamma:\t",np.correlate(self.highAlpha,self.lowGamma)[0]
+        print "High-Alpha|Mid-Gamma:\t",np.correlate(self.highAlpha,self.midGamma)[0]
+        print "High-Alpha|Delta:\t",np.correlate(self.highAlpha,self.delta)[0]
+        print "High-Alpha|Theta:\t",np.correlate(self.highAlpha,self.theta)[0]
+        print "\n"
+        
+        print "Low-Beta|High-Beta:\t",np.correlate(self.lowBeta,self.highBeta)[0]
+        print "Low-Beta|Low-Gamma:\t",np.correlate(self.lowBeta,self.lowGamma)[0]
+        print "Low-Beta|Mid-Gamma:\t",np.correlate(self.lowBeta,self.midGamma)[0]
+        print "Low-Beta|Delta:\t",np.correlate(self.lowBeta,self.delta)[0]
+        print "Low-Beta|Theta:\t",np.correlate(self.lowBeta,self.theta)[0]
+        print "\n"
+        
+        print "High-Beta|Low-Gamma:\t",np.correlate(self.highBeta,self.lowGamma)[0]
+        print "High-Beta|Mid-Gamma:\t",np.correlate(self.highBeta,self.midGamma)[0]
+        print "High-Beta|Delta:\t",np.correlate(self.highBeta,self.delta)[0]
+        print "High-Beta|Theta:\t",np.correlate(self.highBeta,self.theta)[0]
+        print "\n"
+        
+        print "Low-Gamma|Mid-Gamma:\t",np.correlate(self.lowGamma,self.midGamma)[0]
+        print "Low-Gamma|Delta:\t",np.correlate(self.lowGamma,self.delta)[0]
+        print "Low-Gamma|Theta:\t",np.correlate(self.lowGamma,self.theta)[0]
+        print "\n"
+        
+        print "Mid-Gamma|Delta:\t",np.correlate(self.midGamma,self.delta)[0]
+        print "Mid-Gamma|Theta:\t",np.correlate(self.midGamma,self.theta)[0]
+        print "\n"
+        
+        print "Delta|Theta:\t",np.correlate(self.delta,self.theta)[0]
+        print "\n\n"
 
 s = "/home/suryo/MyStuff/MyDevelopment/Cerebro/Data Tools/Data/CerebroCSV/datasets/hawkrattle/hawkrattle"
 n = 10
 hawkrattle = dataSetEssentials(s,n,"HAWKRATTLE")
 hawkrattle.showEssentialStats()
+hawkrattle.attributeCorrelation()
 
 s = "/home/suryo/MyStuff/MyDevelopment/Cerebro/Data Tools/Data/CerebroCSV/datasets/vatican/vatican"
 n = 10
-hawkrattle = dataSetEssentials(s,n,"VATICAN")
-hawkrattle.showEssentialStats()
+vatican = dataSetEssentials(s,n,"VATICAN")
+vatican.showEssentialStats()
+vatican.attributeCorrelation()
 
 s = "/home/suryo/MyStuff/MyDevelopment/Cerebro/Data Tools/Data/CerebroCSV/datasets/visionary/visionary"
 n = 10
-hawkrattle = dataSetEssentials(s,n,"VISIONARY")
-hawkrattle.showEssentialStats()
+visionary = dataSetEssentials(s,n,"VISIONARY")
+visionary.showEssentialStats()
+visionary.attributeCorrelation()
